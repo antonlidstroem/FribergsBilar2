68b2a823788f1434d56508a316d40c57b7de95db
Anton Lidström <antonlidstroem@outlook.com>
2025-10-29 11:43:06 +0100

    Separated DAL from Client. Still something wrong with DB connection

---
+++ DAL/Classes/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DAL.Classes;
+
+namespace DAL.Classes
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+
+        public List<CarImage> CarImages { get; set; } = new();
+       
+        public string Brand { get; set; }
+        
+        public string Model { get; set; }
+        [Range(1900, 2100)]
+        public int Year { get; set; }
+        [Range(0, double.MaxValue)]
+        public decimal PricePerDay { get; set; }
+        public bool Available { get; set; }
+
+        public string CarDescription { get; set; }
+    }
+}
+
---
+++ DAL/Repositories/IOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿//using System.Collections.Generic;
+//using System.Threading.Tasks;
+using DAL.Classes;
+
+
+namespace DAL.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<IEnumerable<Order>> GetAllOrdersAsync();
+        Task<Order?> GetOrderByIdAsync(int id);
+        Task AddOrderAsync(Order order);
+        Task UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(int id);
+        Task<bool> IsCarBookedAsync(int carId, DateTime startDate, DateTime endDate);
+        Task<bool> OrderExistsAsync(int id);
+
+        Task<decimal> GetTotalEarningsForCarAsync(int id);
+        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+
+    }
+}
