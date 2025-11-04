8800b39b7270b9d6869f2aa4f73621e6d971019c
Anton Lidström <antonlidstroem@outlook.com>
2025-11-04 13:51:44 +0100

    Ended in the middle of trying to get create order to work

---
+++ FribergsApi/Models/OrderDto.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FribergsApi.Models
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+
+        [Required]
+        public int CarId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        // (Valfritt) för enkelhet i vyn:
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+    }
+}
+
+
